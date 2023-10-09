2a0b6933cff7373d851db8568fcf81ed74b029eb
yaggi72 <144679579+yaggi72@users.noreply.github.com>
2023-10-09 18:52:16 +0300

    made calculator and added tests

---
+++ src/lab1/calculator.py
@@ -0,0 +1,36 @@
+def Calculator(num1,num2,action):
+    '''
+
+    :param num1: Число формата целое или с плавающей точкой
+    :param num2: Число формата целое или с плавающей точкой
+    :param action: Возможные действия: "+", "-", "*", "/"
+    :return: Ошибку во вводе или результат действия
+    '''
+    actions = '+-*/'
+    figures = '0123456789.'
+    flaq = True
+    for figure in str(num1):
+        if figure not in figures:
+            flaq = False
+            return 'Ошибка ввода'
+
+    for figure in str(num2):
+        if figure not in figures:
+            flaq = False
+            return 'Ошибка ввода'
+
+    if str(action) not in actions:
+        flaq = False
+        return 'Ошибка ввода'
+
+    if (flaq == True) and action == '+':
+        return num1 + num2
+
+    if (flaq == True) and action == '-':
+        return num1 - num2
+
+    if (flaq == True) and action == '*':
+        return num1 * num2
+
+    if (flaq == True) and action == '/':
+        return num1 / num2
--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -1,7 +1,11 @@
 import unittest
-
-class CalculatorTestCase(unittest.TestCase):
-
-    # Тест для проверки работы, можно удалить
-    def test_one(self):
-        self.assertEquals(1, 1)
+import sys
+from ../..src.lab1.calculator import Calculator
+class TestCalculator(unittest.TestCase):
+    def test_Calculator(self):
+        self.assertEqual(Calculator(5, 2, '+'), 7)
+        self.assertEqual(Calculator(5.0, 2, '-'), 3.0)
+        self.assertEqual(Calculator(5, 2.0, '*'), 10)
+        self.assertEqual(Calculator(5, 2, '/'), 2.5)
+        self.assertEqual(Calculator(5, 2, '123'), 'Вы ошиблись во вводе')
+        self.assertEqual(Calculator(5, 'asds54sdf', '*'), 'Вы ошиблись во вводе')
